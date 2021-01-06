51b9acea0e4db05cf3553ef12aa24eefb14f0302
bqqbarbhg <samuli.1995@hotmail.com>
2021-01-06 20:06:36 +0200

    Ship it

--- misc/gcsim.py
+++ misc/gcsim.py
@@ -1,5 +1,7 @@
 from random import choice
 import random
+from itertools import count
+import sys
 
 sim_verbose = False
 
@@ -24,6 +26,8 @@ def walk_g(obj):
             mark_g(ref)
         elif ref.gen[0] == "N":
             mark_g(ref)
+    if sim_verbose:
+        print(f"Walk G {obj} {obj.props}")
 
 def walk_n(obj):
     for ref in obj.props.values():
@@ -31,8 +35,11 @@ def walk_n(obj):
             mark_g(ref)
         elif ref.gen == "N3":
             mark_n(ref)
+    if sim_verbose:
+        print(f"Walk N {obj} {obj.props}")
 
 all_objs = []
+scanning_stacks = False
 
 class Object:
     def __init__(self, gen):
@@ -48,7 +55,7 @@ class Object:
     def set(self, name, obj):
         global sim_verbose
 
-        prev = self.props.get(obj)
+        prev = self.props.get(name)
         self.props[name] = obj
 
         if sim_verbose:
@@ -56,18 +63,24 @@ class Object:
 
         if self.gen[0] == "G" and obj.gen[0] == "N":
             mark_g(obj)
+        # elif obj.gen == "N3" and scanning_stacks:
+            # mark_n(obj)
 
         if prev and prev.gen == "G2":
             mark_g(prev)
-        elif prev and prev.prev == "N3":
+        elif prev and prev.gen == "N3":
             mark_n(prev)
 
 class Thread:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.stack = []
+        self.alloc_gen = "N3"
+    
+    def __repr__(self):
+        return self.name
 
 root = Object("G3")
-alloc_gen = "N3"
 names = list("xyzw")
 
 def thread_sim(func):
@@ -79,16 +92,20 @@ def sim_gc_g():
 
 def sim_gc_n():
     if queue_n:
-        walk_n(queue_g.pop())
+        walk_n(queue_n.pop())
 
 @thread_sim
 def sim_new(t):
-    t.stack.append(Object(alloc_gen))
+    t.stack.append(Object(t.alloc_gen))
+    if sim_verbose:
+        print(f".. {t} new: {t.stack}")
 
 @thread_sim
 def sim_dup(t):
     if not t.stack: return
     t.stack.append(t.stack[-1])
+    if sim_verbose:
+        print(f".. {t} dup: {t.stack}")
 
 @thread_sim
 def sim_load(t):
@@ -96,25 +113,33 @@ def sim_load(t):
     obj = t.stack.pop()
     if obj.props:
         t.stack.append(choice(list(obj.props.values())))
+    if sim_verbose:
+        print(f".. {t} load: {t.stack}")
 
 @thread_sim
 def sim_store(t):
     if len(t.stack) < 2: return
     obj = t.stack.pop()
     t.stack[-1].set(choice(names), obj)
+    if sim_verbose:
+        print(f".. {t} store: {t.stack[-1]} {t.stack[-1].props}")
 
 @thread_sim
 def sim_load_root(t):
     if root.props:
         t.stack.append(choice(list(root.props.values())))
+        if sim_verbose:
+            print(f".. {t} load root: {t.stack}")
 
 @thread_sim
 def sim_store_root(t):
     if not t.stack: return
     obj = t.stack.pop()
     root.set(choice(names), obj)
+    if sim_verbose:
+        print(f".. {t} store root: {root.props}")
 
-threads = [Thread() for _ in range(4)]
+threads = [Thread(f"t{n}") for n in range(4)]
 
 sims = ([]
     + [sim_gc_g] * 2
@@ -144,56 +169,73 @@ def check_retain(work):
             work.append(prop)
     return True
 
-def mark_stacks(threads):
-    for t in threads:
-        for s in t.stack:
-            if s.gen != "G3":
-                mark_g(s)
+def mark_stack(t):
+    t.alloc_gen = "G3"
+    for s in t.stack:
+        if s.gen != "G3":
+            mark_g(s)
+    if sim_verbose:
+        print(f".. {t} mark stack: {t.stack}")
 
-def simulate(n, verbose):
+def simulate(seed, verbose):
     global root
-    global alloc_gen
     global sim_verbose
+    global scanning_stacks
 
     all_objs.clear()
-    random.seed(n)
+    random.seed(seed)
     sim_verbose = verbose
     next_id = 1
+    scanning_stacks = False
+
+    steps = random.choices(
+        [100, 1000, 10000],
+        [100, 10, 1],
+    )[0]
+
+    steps = 15
 
     root = Object("G3")
     for t in threads:
         t.stack.clear()
+        t.alloc_gen = "N3"
 
-    alloc_gen = "N3"
-    for n in range(200):
+    for n in range(steps):
         choice(sims)()
 
-    alloc_gen = "G3"
-    mark_stacks(threads)
+    scanning_stacks = True
+
+    for t in threads:
+        mark_stack(t)
+        for n in range(steps):
+            choice(sims)()
+
     while queue_g or queue_n:
         while queue_g or queue_n:
             choice(sims_gc)()
-        mark_stacks(threads)
-        
-    if verbose:
-        return True
 
     roots = [root]
     for t in threads:
         roots += t.stack
+
+    if verbose:
+        assert not check_retain(roots)
+        return True
+
     if not check_retain(roots):
-        print(f"Fail at attempt {n}")
+        print(f"Fail at attempt {seed}")
 
         for t in threads:
             print(t.stack)
         for obj in all_objs:
-            print(f"{obj}: {obj.props}")
+            print(f"{obj} => {obj.props}")
 
-        return simulate(n, True)
+        return simulate(seed, True)
     return False
 
-for n in range(1000000):
-    if n % 1000 == 0:
+start = int(sys.argv[1] if len(sys.argv) > 1 else 0)
+for n in count(start=start, step=100):
+    if (n//100) % 10000 == 0:
         print(".", end="", flush=True)
     if simulate(n, False):
         break
